d8e7f0f24ab400058bfaff2e33699a6be074089c
Alexandr Kuntsevich <1st.Riddle@gmail.com>
2024-06-17 15:06:27 +0300

    C3 lesson_5, homework_5.1

---
+++ lesson_5/ greetings.js
@@ -0,0 +1,11 @@
+let number = 13;
+
+if (number >= 6 && number < 12) {
+    console.log("Добрий ранок");
+} else if (number >= 12 && number < 18) {
+    console.log("Добрий день");
+} else if (number >= 18 && number < 24) {
+    console.log("Добрий вечір");
+} else {
+    console.log("Доброї ночі");
+}
